cc45c288808f2bd61bf931f4fbd673132ad3bd6b
Quinn Dougherty <quinn.dougherty92@gmail.com>
2019-05-30 18:44:41 -0400

    unit tests

--- test-nthprime.py
+++ test-nthprime.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
 from nthprime import nthprime
-from unittest import TestCase
+import unittest
 
-class KnownNthPrime(TestCase):
+class KnownNthPrime(unittest.TestCase):
 
     def test1(self):
         self.assertEqual(11, nthprime(5))
 
     def test2(self):
         self.assertEqual(13, nthprime(6))
+
+if __name__ == "__main__":
+    unittest.main()
